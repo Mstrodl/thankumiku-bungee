2dfab8470bff4b1b376386681167cb472c5eb5a1
Mary Strodl <ipadlover8322@gmail.com>
2020-07-01 16:28:45 -0400

    Initial commit

---
+++ src/main/java/tech/coolmathgames/thankumiku/ThankuMiku.java
@@ -0,0 +1,10 @@
+package tech.coolmathgames.thankumiku;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ThankuMiku extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Ready to thank Miku!");
+    }
+}
